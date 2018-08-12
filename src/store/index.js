39c4fd2440eadf0065252e5977359cf6f053a9cb
Skuznetz <s.kuznetzov@mail.ru>
2018-08-12 18:25:43 +0400

    создадим редюсер filter

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 
-import rootReducer from '../reducers/todos.js';
+import rootReducer from '../reducers';
 
-// const createStore = (reducer) => {
-//     let state;
-//     let listeners = [];
-//
-//     const getState = () => state;
-//
-//     const dispatch = (action) => {
-//         state = reducer(state, action);
-//         listeners.forEach(listener => listener());
-//     };
-//
-//     const subscribe = (listener) => {
-//         listeners.push(listener);
-//
-//         return () => {
-//             listeners = listeners.filter(l => l !== listener);
-//         }
-//     };
-//
-//     dispatch({});
-//
-//     return { getState, dispatch, subscribe };
-// };
+
 
 export default createStore(rootReducer);
