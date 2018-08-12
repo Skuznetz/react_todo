93d317a108147e0d289846cc5a56ea2de25c5eac
Skuznetz <s.kuznetzov@mail.ru>
2018-08-12 18:17:33 +0400

    объединим редюсеры

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 
-import rootReducer from '../reducers';
+import rootReducer from '../reducers/todos.js';
 
 // const createStore = (reducer) => {
 //     let state;
